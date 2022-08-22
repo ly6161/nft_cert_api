2c3c30a44df20d9a641994fb2acd363a11e11ee8
ly6161 <ly6161616161@gmail.com>
2022-08-23 06:08:29 +0800

    change port, test ssl

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ global.global_app = app;
 require('./routes/CertNftRoutes.js')
 require('./routes/UserRoutes.js')
 
-const port = 3000
+const port = 8080
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/test_ssl', (req, res) => {
+  res.send('Hello world using SSL')
 })
 
 app.listen(port, () => {
